9662748bad8ae48bd14276ce418003c4d480de18
Aakash Kapdi <aakash.work17@gmail.com>
2022-08-11 16:16:08 +0530

    add pagination

--- onTheMarket/onthemarket/onthemarket/spiders/commercial_sale.py
+++ onTheMarket/onthemarket/onthemarket/spiders/commercial_sale.py
@@ -7,7 +7,6 @@ from scrapy.selector import Selector
 import urllib
 from datetime import date
 
-import urllib3
 
 # OnTheMarket scraper class
 class CommercialSale(scrapy.Spider):
@@ -88,25 +87,47 @@ class CommercialSale(scrapy.Spider):
         for card in cards:
             listing=card.xpath("div/div[@class='otm-PropertyCardMedia']//a/@href").get()
             yield response.follow(url=listing,headers=self.headers,meta={'postcode':postcode,'filename':filename,'count':count},callback=self.parse_listing)
-            break
+        
+        # pagination logic
+        try:
+            self.current_page+=1
+            try:
+                total_pages=max([int(link) for link in response.xpath("//div[@class='otm-Pagination noprint otm-new']//ul//a/text()").getall()])
+            except:
+                total_pages=1
+           
+            #create next page link
+            self.params['page']=self.current_page
+            next_page_url=self.base_url+postcode+'/?'+ urllib.parse.urlencode(self.params)
+            print("\n\n%s|%s \n\n" %(int(self.current_page),total_pages) )
+            
+            if self.current_page < total_pages:
+                yield response.follow(url=next_page_url,headers=self.headers,meta={'postcode':postcode,'filename':filename,'count':count},callback=self.parse_links)
+                
+                
+        except:
+            pass 
+        
         
     # Parse property listings
     def parse_listing(self,response):
         # extract forwarded data
         postcode=response.meta.get('postcode')
         filename=response.meta.get('filename')
-        
+        count=response.meta.get('count')
         #extracting features
         features={
-            'id':'',
-            'postcode':postcode,
-            'title':response.xpath("//h1/text()").get(),
-            'Address':response.xpath("//h1/following-sibling::div[1]/text()").get(),
-            'price': response.xpath("//section[@class='main-col']//div[@class='otm-Price']/*/text()").extract()[-1].split("|")[0],
-            'feature_list':response.xpath("//section[@class='otm-FeaturesList mb-12']//li/text()").extract(),
-            'photo_links':response.xpath("//li/picture/img/@src").extract()
-     #       'description':response.xpath("//div[@class='description-truncate']/div/*[descendant::text()]/text()").extract()
-        }
+                        'id':response.url.split("?")[0].split("/")[-2] ,
+                        'postcode':postcode,
+                        'title':response.xpath("//h1/text()").get(),
+                        'Address':response.xpath("//h1/following-sibling::div[1]/text()").get(),
+                        'price': response.xpath("//section[@class='main-col']//div[@class='otm-Price']/*/text()").extract()[-1].split("|")[0].replace("\u00a3",'').strip(),
+                        'feature_list':response.xpath("//section[@class='otm-FeaturesList mb-12']//li/text()").extract(),
+                        'photo_links':response.xpath("//div[@class='hero']//img/@src").getall(),
+                        'agent_name':response.xpath("//div[@class='agent-info-overview']//h2/text()").get(),
+                        'agent_phone':response.xpath("//div[@class='agent-info-contact']/div/text()").get(),
+                        'description':response.xpath("//div[@class='description-truncate']/div/*[descendant::text()]/text()").extract()
+            }
         
         print(json.dumps(features,indent=2))
         
